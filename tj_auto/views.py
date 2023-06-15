848c1cd7db4ee8391f93284f99d872126a8e290a
allesistbesser <mtepebas46@gmail.com>
2023-06-15 22:35:42 +0200

    add index and static files

--- tj_auto/views.py
+++ tj_auto/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Cars, Category
 # Create your views here.
+
+def home(request):
+  featured = Cars.objects.filter(status="published", featured="yes")
+  categorys = Category.objects.all()
+  context = {
+       'featured': featured,
+       'category': categorys,
+   }
+  return render(request, 'index.html', context)
